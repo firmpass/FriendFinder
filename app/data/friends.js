0f50239b353456e24f31ae159ff74e124f8342c4
Eric "ET" Thornton <firmonomics@gmail.com>
2018-05-11 23:55:49 -0400

    added modal view unfin, ajax call, submit button logic, pushed object to the location.window.origin.

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+
+friendData = [
+    {
+        name: "Ethan Thornton",
+        photoURL: "Your Photo",
+        scores: ["1","2","3","5","5","4","3","2","1","2"]
+        
+    }
+];
+
+module.exports = friendData;
+
+//Connect apiRoutes successfully to :data/friends path to display Json data!
+
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
  
+require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
 
